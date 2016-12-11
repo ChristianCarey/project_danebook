92773496c5d9ac16961bb5c9077942798975cd4f
Christian Carey <christianrcarey@gmail.com>
2016-12-10 21:01:17 -0500

    users can delete comments.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,6 +11,13 @@ class CommentsController < ApplicationController
   end
 
   def destroy
+    @comment = Comment.find(params[:id])
+    require_current_user
+    if @comment.destroy
+      redirect_to :back, success: "Comment removed."
+    else
+      redirect_to :back, danger: "We can't delete that comment."
+    end
   end
 
   private
@@ -22,5 +29,11 @@ class CommentsController < ApplicationController
   def extract_commentable
     resource, id = request.path.split('/')[1,2]
     resource.singularize.classify.constantize.find(id)
+  end
+
+  def require_current_user
+    unless @comment.author == current_user
+      redirect_to :back, danger: "That's not yours to delete."
+    end
   end
 end
--- app/presenters/comment_presenter.rb
+++ app/presenters/comment_presenter.rb
@@ -1,4 +1,11 @@
 class CommentPresenter < BasePresenter
   include LikablePresenter
   include CommentablePresenter
+
+  def delete_link
+    if current_user == @object.author
+      link_to 'Delete', comment_path(@object), method: :delete, 
+                                               class: 'delete-comment'
+    end
+  end
 end
