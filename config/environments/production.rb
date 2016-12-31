4c8aff3409f5220db9a294add5ce4952df389f56
Christian Carey <christianrcarey@gmail.com>
2016-12-31 01:36:45 -0500

    update email host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ Rails.application.configure do
   }
 
   config.action_mailer.default_url_options = {
-    :host => 'pure-lake-60456.herokuapp.com/',
+    :host => 'https://the-real-danebook.herokuapp.com/',
   }
   
   config.cache_classes = true
